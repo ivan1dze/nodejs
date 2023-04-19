24ce561878bca743f68554e7683df826e1940387
ivan1dze <50000311+ivan1dze@users.noreply.github.com>
2023-04-19 19:50:12 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 exports.helloWorld = (req, res) => {
-  const message="<font color='blue'>СloudFunction of quickejeembo</font><br><b>App Version 1.1</b>";
+  const message="<font color='blue'>СloudFunction of quickejeembo</font><br><b>App Version 1.01</b>";
   res.status(200).send(message);
 };
